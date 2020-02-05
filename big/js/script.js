83079008cf8afbda9e5b5b43bcf800eb0e21a8e4
KronosEmporium <KronosEmporium12@gmail.com>
2020-02-05 10:24:06 -0800

    deletes all files for new project

--- big/js/script.js
+++
@@ -1,9 +0,0 @@
-(function($){
-    $(document).ready(function(){
-        $("#cssmenu").menumaker({
-            title: "Menu",
-            breakpoint: 827,
-            format: "multitoggle"
-        });
-    });
-})(jQuery);
