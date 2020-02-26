69223a066e69acfe5deb25207e60015ffe4ea5c9
yan17asn <248764378@qq.com>
2020-02-25 21:52:41 -0800

    Add files via upload

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,8 @@
 <!-- START OF FOOTER -->
 
             <footer>
-                <p>Icons made by <a href="https://www.flaticon.com/authors/photo3idea-studio" title="photo3idea_studio">photo3idea_studio</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a></p>
+                <p><img src="images/github.png"><a href="https://github.com/KronosEmporium/KronosEmporium.github.io" target="_blank">Github Repo</a>&nbsp;&nbsp;<img src="images/doc.png"><a href="https://docs.google.com/document/d/1OKhyfA_zxMg11k7WRSh5MuwwuzRvBkhILPhTxJJr1D4/edit?usp=sharing" title="Flaticon" target="_blank">Project Google Doc</a></p>
+                <p id='copyright'>Copyright &copy 2020 ITC250 Group Project</p>
             </footer>
 
         </div>
